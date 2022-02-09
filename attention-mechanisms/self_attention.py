5bb8abd5022dbd67300e5ecc6a5dd5edb0844983
sajith.rahim <sajith.rahim@outlook.com>
2022-02-10 00:41:37 +0530

    fix: dimensions

--- attention-mechanisms/self_attention.py
+++ attention-mechanisms/self_attention.py
@@ -19,9 +19,12 @@ class ScaledDotSelfAttention(nn.Module):
         super(ScaledDotSelfAttention, self).__init__()
 
         self.n_heads = n_heads
-        self.dim = dim
+        self.d_model = dim
 
         assert dim % n_heads == 0, "Dimension has to divisible by n_heads iorder to split!"
+        
+        # d_k = d_v
+        self.dim = dim//n_heads
 
         self.Wq = nn.Linear(dim, n_heads * dim)
         self.Wk = nn.Linear(dim, n_heads * dim)
@@ -32,43 +35,43 @@ class ScaledDotSelfAttention(nn.Module):
     def forward(self, queries, keys, values, batch_size=None):
         r"""
 
-        :param queries: [batch_size, n_queries, dim]
-        :param keys: [batch_size, n_keys, dim]
-        :param values: [batch_size, n_keys, dim]
+        :param queries: [batch_size, query_len, dim]
+        :param keys: [batch_size, key_len, dim]
+        :param values: [batch_size, key_len, dim]
         :param batch_size: batch_size
         :return: scaled attn product
         """
-        batch_size, n_queries = queries.shape[:2]
+        batch_size, query_len = queries.shape[:2]
 
-        n_keys = keys.shape[1]
+        key_len = keys.shape[1]
 
         q = self.Wq(queries)
         k = self.Wk(keys)
         v = self.Wv(values)
 
-        # Slicing: [batch_size, n_queries, input_dim] -> [batch_size, n_queries, n_heads, dim]
-        q = q.view(batch_size, n_queries, self.n_heads, self.dim)
-        # [batch_size, n_queries, n_heads, dim] -> [batch_size,n_heads,n_queries,dim]
+        # Slicing: [batch_size, query_len, input_dim] -> [batch_size, query_len, n_heads, dim]
+        q = q.view(batch_size, query_len, self.n_heads, self.dim)
+        # [batch_size, query_len, n_heads, dim] -> [batch_size,n_heads,query_len,dim]
         q = q.permute(0, 2, 1, 3)
 
         # Slicing
-        k = k.view(batch_size, n_keys, self.n_heads, self.dim)
-        # [batch_size, n_keys, n_heads, dim] -> [batch_size,n_heads,n_keys,dim]
+        k = k.view(batch_size, key_len, self.n_heads, self.dim)
+        # [batch_size, key_len, n_heads, dim] -> [batch_size,n_heads,dim,key_len] : K^T
         k = k.permute(0, 2, 3, 1)
 
-        attn = torch.matmul(q, k) / torch.sqrt(torch.tensor(float(self.dim)))  # [batch_size,n_heads,n_queries,n_keys]
+        attn = torch.matmul(q, k) / torch.sqrt(torch.tensor(float(self.dim)))  # [batch_size,n_heads,query_len,key_len]
 
         attn = torch.softmax(attn, -1)
 
         # plot(attn)
 
-        v = v.view(batch_size, n_keys, self.n_heads, self.dim)
-        # [batch_size, n_keys, n_heads, dim] -> [batch_size,n_heads,n_keys,dim]
+        v = v.view(batch_size, key_len, self.n_heads, self.dim)
+        # [batch_size, key_len, n_heads, dim] -> [batch_size,n_heads,key_len,dim]
         v = v.permute(0, 2, 1, 3)
 
         v_ = torch.matmul(attn, v)
-        # [batch_size, n_queries, n_heads * dim]
-        v_ = v_.permute(0, 2, 1, 3).contiguous().view(batch_size, n_queries, self.n_heads * self.dim)
+        # [batch_size, query_len, n_heads * dim]
+        v_ = v_.permute(0, 2, 1, 3).contiguous().view(batch_size, query_len, self.n_heads * self.dim)
 
-        out = self.out(v_)  # (batch_size, n_queries, dim)
+        out = self.out(v_)  # (batch_size, query_len, dim)
         return out
